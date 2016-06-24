e24f0f961601dd7a2302f2a41074b925a757a5c0
Shuvo Khan <mrshuvo.97@gmail.com>
2016-06-25 02:01:40 +0600

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,17 @@
+
+
+
+
+$(document).ready(function() {
+ 
+  $("#owl-demo").owlCarousel({
+ 
+      autoPlay: 3000, //Set AutoPlay to 3 seconds
+ 
+      items : 6,
+      itemsDesktop : [1199,6],
+      itemsDesktopSmall : [979,6]
+ 
+  });
+ 
+})
